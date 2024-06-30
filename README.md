5de1253bdd737dc907dcadc96cf874bca8d24981
Carlo <carlo.nicolo@hotmail.com>
2024-06-30 23:40:23 +0200

    Adding composer, and ssl support for mongodb authentication

--- README.md
+++ README.md
@@ -11,6 +11,10 @@ Three containers with persisten data and a Dockerfile used for installing and co
 ```bash
 $ docker-compose up -d --build
 ```
+**Composer add**:  
+```$ docker exec -it php-apache bash``
+```# composer require mongodb/mongodb ```
+
 
 ### MongoDB
 
@@ -25,14 +29,20 @@ db.createCollection('friends')
 db.friends.insert({name: "John", age: 23})
 db.friends.insert({name: "Jack", age: 21})
 db.friends.insert({name: "Bob", age: 22})
+db.friends.insert({name: "Mary", age: 20})
 db.friends.find()
-
 ```
 
 ### Useful commands
 
 Show all documents with age greater than 22:  
 ```db.friends.find({age: {$gt: 23}})```
+
+Update command:
+db.friends.update(search term, update term)  
+```db.friends.update({}, {$set: {gender: "m"}})```  
+```db.friends.update({}, {$set: {gender: "m"}}, {multi: true})```  
+```db.friends.update({name: "Mary"}, {$set: {gender: "f"}}, {multi: true})```
 
 ### Apache PHP
 
@@ -42,3 +52,17 @@ Apache will execute the PHP script in your index.php file and connect to the Mon
 ### Mongo Express
 
 Access Mongo Express: http://localhost:8081/
+
+## Useful tips about 
+
+### DB Relationships  
+
+- **One to One**
+- **One to Many**
+- **Many to Many**
+
+### Indexes
+
+```db.comments.ensureIndex({"author": 1})```  
+```db.comments.getIndexes()```  
+
--- app/index.php
+++ app/index.php
@@ -1,9 +1,47 @@
 <?php
 
-try {
-    $mongoClient = new MongoDB\Driver\Manager("mongodb://admin:pass@mongodb:27017/");
+require 'vendor/autoload.php';
 
-    echo "Connected to MongoDB successfully!";
-} catch (Exception $e) {
-    echo "Failed to connect to MongoDB: " . $e->getMessage();
+try {
+
+    $client = new MongoDB\Client('mongodb://admin:pass@mongodb:27017/');
+    $friends = $client->mydb->friends;
+    $result = $friends->find(array(), array('projection' => array('_id' => false)));
+
+    $data = iterator_to_array($result);
+    //echo '<pre>' , var_dump($data) , '</pre>';
+
+
+} catch (\MongoDB\Driver\Exception\Exception $e) {
+    echo $e;
 }
+?>
+
+<html>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <?php foreach ($data[0] as $key => $value) : ?>
+                    <th>
+                        <?php echo $key;  ?>
+                    </th>
+                <?php endforeach; ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($data as $entry) : ?>
+                <tr>
+                    <?php foreach ($entry as $key => $value) : ?>
+                        <td>
+                            <?php echo $value; ?>
+                        </td>
+                    <?php endforeach; ?>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
